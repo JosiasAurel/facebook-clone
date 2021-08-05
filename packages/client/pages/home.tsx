1819483a9b005b76123727d1674e89f279c4fa78
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-05 18:11:57 +0100

    start creating home dashboard

---
+++ packages/client/pages/home.tsx
@@ -0,0 +1,14 @@
+
+import React, { FunctionComponent } from "react";
+
+import Header from "../components/Header";
+
+const HomeDashboard: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+            <h2>Nothing</h2>
+        </div>
+    )
+}
+
+export default HomeDashboard;
