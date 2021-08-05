68a92bb44b1110fcd90056e075f2702d9d5f0539
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-05 20:00:55 +0100

    set margin on home

--- packages/client/pages/home.tsx
+++ packages/client/pages/home.tsx
@@ -2,12 +2,27 @@
 import React, { FunctionComponent } from "react";
 
 import Header from "../components/Header";
+import FirstSectionCard from "../components/FirstSectionCard";
+
+import styles from "../styles/home.module.css";
 
 const HomeDashboard: FunctionComponent = (): JSX.Element => {
     return (
         <div>
             <Header />
-            <h2>Nothing</h2>
+            <div className={styles.homePageContent}>
+                <div className={styles.firstSection}>
+                    <FirstSectionCard image="covid.png" text="COVID-19 Information Centre" />
+                    <FirstSectionCard image="friends.png" text="Friends" />
+                    <FirstSectionCard image="groups.png" text="Groups" notif={5} />
+                    <FirstSectionCard image="watch.png" text="Watch" notif={9} />
+                    <FirstSectionCard image="events.png" text="Events" notif={9} />
+                    <FirstSectionCard image="memories.png" text="Memories" />
+                    <FirstSectionCard image="save.png" text="Saved" />
+                    <FirstSectionCard image="pages.png" text="Pages" />
+                    <FirstSectionCard image="jobs.png" text="Jobs" />
+                </div>
+            </div>
         </div>
     )
 }
