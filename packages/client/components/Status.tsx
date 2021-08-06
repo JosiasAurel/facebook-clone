db9b21ed75520906106b8e58691f9bbd2cfd9b2e
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-06 03:15:28 +0100

    start status component

---
+++ packages/client/components/Status.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+
+import styles from "../styles/components/components.module.css";
+
+const Status: FunctionComponent = (): JSX.Element => {
+    return (
+        <div className={styles.statusAndAddStatusCard}>
+
+        </div>
+    )
+}
+
+export default Status;
