24fd820006f02dd31d2dc09846cf0e7e3b003565
JosiasAurel <ndjosiasaurel@gmail.com>
2021-08-06 03:32:46 +0100

    make status component

--- packages/client/components/Status.tsx
+++ packages/client/components/Status.tsx
@@ -5,7 +5,9 @@ import styles from "../styles/components/components.module.css";
 const Status: FunctionComponent = (): JSX.Element => {
     return (
         <div className={styles.statusAndAddStatusCard}>
-
+            <img className={styles.statusImage} src="/status.jpg" alt="" />
+            <img className={styles.statusOwnerProfileImage} src="/josias.png" alt="" />
+            <p className={styles.statusOwnerName}>Josias Aurel</p>
         </div>
     )
 }
